e7f33618083ced45a8c2b230c9e63e16b291b04d
matheus souza <ruivorobado@gmail.com>
2021-09-24 16:08:34 -0300

    inciando graphql no projeto

--- testefront/src/index.js
+++ testefront/src/index.js
@@ -2,9 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import ApolloClient from 'apollo-boost'
+import {ApolloProvider} from '@apollo/react-hooks'
+
+
+
+const client = new ApolloClient({
+  uri: "https://api.github.com/graphql",
+  headers: {
+    Authorization: `Bearer ${process.env.REACT_APP_GIT_HUB_TOKEN}`
+  }
+})
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
